93f19b1ccb9aef79b69d25be036539fb7c07602d
Ryan Bohannon <ryanbrohannon@gmail.com>
2016-03-30 17:19:47 -0600

    update routes

--- express/routes/index.js
+++ express/routes/index.js
@@ -7,7 +7,18 @@ var stormpath = require('express-stormpath');
 
 // Render the home page.
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Home', user: req.user });
+  res.render('animations', { title: 'Home', user: req.user });
 });
+
+
+router.get('/modal', function(req, res) {
+  res.render('modal_example', { title: 'Home', user: req.user });
+});
+
+router.get('/admin', function(req, res) {
+  res.render('admin', { title: 'Home', user: req.user });
+});
+
+
 
 module.exports = router;
