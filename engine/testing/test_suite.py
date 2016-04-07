4db198e9f47524dbe29a9023516fdaa538bb3b9c
Jared Jolton <jared.jolton@gmail.com>
2016-04-07 17:26:54 -0600

    pair programming with connor and wil: working on test_suite, several cases added

--- engine/testing/test_suite.py
+++ engine/testing/test_suite.py
@@ -1,6 +1,11 @@
 #!usr/bin/env python
 
 import unittest
+from sys import path
+
+path.append('../process/')
+
+from document import Document
 
 class EngineTestCase(unittest.TestCase):
     
@@ -20,7 +25,32 @@ class EngineTestCase(unittest.TestCase):
 
     def test_raw_text_processing(self):
         text = "test"
-        d = Document(user, text)
-        self.assertEqual(d.raw, text, "raw text conversion failed")
+        with open("../process/tmp_test_file.txt", "w") as test_file:
+            test_file.write(text)
+        d = Document("tmp_test_file.txt", "testuser")
+        self.assertEqual(type(d.raw), type(text), "raw text conversion failed, not correct type")
+
+    def test_stats_processing(self):
+        text = "test"
+        with open("../process/tmp_test_file.txt", "w") as test_file:
+            test_file.write(text)
+        d = Document("tmp_test_file.txt", "testuser")
+        self.assertEqual(type(d.stats), dict, "stats processing failed, not correct type")
+
+    def test_sentence_tokenizing(self):
+        text = "This is a test sentence."
+        with open("../process/tmp_test_file.txt", "w") as test_file:
+            test_file.write(text)
+        d = Document("tmp_test_file.txt", "testuser")
+        d.preprocess_text()
+        self.assertEqual(d.preprocessed['sentences'], 1, "sentence tokenizing failed, incorrect number of sentences")
+
+    def test_word_tokenizing(self):
+        text = "This is a test sentence."
+        with open("../process/tmp_test_file.txt", "w") as test_file:
+            test_file.write(text)
+        d = Document("tmp_test_file.txt", "testuser")
+        d.preprocess_text()
+        self.assertEqual(d.preprocessed['tokens'], 6, "word tokenizing failed, incorrect number of tokens")
 
 if __name__ == '__main__': unittest.main()
