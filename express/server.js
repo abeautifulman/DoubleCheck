644a6bbc0352026d974b8e7f8002b8bdcc8212b5
Ryan Bohannon <ryanbrohannon@gmail.com>
2016-04-21 09:11:25 -0600

    added bodyparser to server.js DOH

--- express/server.js
+++ express/server.js
@@ -38,6 +38,7 @@ app.use(favicon());
 app.use(logger('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
 
