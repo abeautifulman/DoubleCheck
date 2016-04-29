aef11d1bd0a8c87b808897f535aa5b1e386cd407
Jared Jolton <jared.jolton@gmail.com>
2016-04-28 20:45:19 -0600

    about page styling, wikipedia api

--- engine/process/wikitools.py
+++ engine/process/wikitools.py
@@ -1,9 +1,10 @@
 #!usr/bin/env python
 
-from glob        import glob
-from nltk.corpus import wordnet as wn
+import wikipedia # access and parse data from wikipedia
+from glob        import glob # list the contents of directories
+from nltk.corpus import wordnet as wn # generate synsets
 
-import click 
+import click # progress bars
 
 class Topic:
 	'''
@@ -11,29 +12,36 @@ class Topic:
 	'''
 	def __init__(self, topic):
 
-		self.text = self.findTopic(topic)
+	    #self.text = self.findTopic(topic)
+            self.topic = topic
+
+        def synsets(self, topic):
+            # assess wordnet to determine all similar topic to search for
+            topics = []
+            for syn in wn.synsets(topic):
+                for word in [str(lemma.name()) for lemma in wn.synset(syn.name()).lemmas()]:
+                    topics.append(word.replace('_',' '))	
+            return list(set(topics)) # remove duplicates
 
 	def findTopic(self, topic):
-
-		# first, assess wordnet to determine all similar topic to search for
-		topics = []
-		for syn in wn.synsets(topic):
-			for word in [str(lemma.name()) for lemma in wn.synset(syn.name()).lemmas()]:
-				topics.append(word.replace('_',' '))	
-		topics = list(set(topics)) # remove duplicates
-
-		# search the entire database for the topics
-		print "searching for:"
-		for word in topics:
-			print word
-		texts = []
-		with click.progressbar(glob('wikipedia/*'), label='scanning corpus...') as wikipedia:
-			for folder in wikipedia:
-				for wikidump in glob(folder + "/*"):
-					with open(wikidump, 'r') as dump:
-						raw = dump.readlines()
-						for line in raw:
-							for word in topics:
-								if word in line:
-									texts.append(line)
-		return texts
+            # search the entire database for the topics
+            topics = self.synsets(topic)
+            print "searching for:"
+            for word in topics:
+                print word
+            texts = []
+            with click.progressbar(glob('wikipedia/*'), label='scanning corpus...') as wikipedia:
+                for folder in wikipedia:
+                    for wikidump in glob(folder + "/*"):
+                        with open(wikidump, 'r') as dump:
+                            raw = dump.readlines()
+                            for line in raw:
+                                for word in topics:
+                                    if word in line:
+                                        texts.append(line)
+	    return texts
+        
+        def findTopicOnline(self):
+            topics = self.synsets(self.topic)
+            for topic in topics:
+                print wikipedia.summary(topic)
--- engine/sparknotes/crawler.py
+++ engine/sparknotes/crawler.py
@@ -26,31 +26,36 @@ class SparknotesCrawler:
             for syn in wn.synsets(term):
                 for word in [str(lemma.name()) for lemma in wn.synset(syn.name()).lemmas()]:
                     topics.append(word.replace('_',' '))	
-            print "\n==== found"
-            for t in list(set(topics)):
-                print "======", t
+            #print "\n==== found"
+            #for t in list(set(topics)):
+            #    print "======", t
             all_topics += list(set(topics)) # remove duplicates and add to master list
         return all_topics
 
     def crawl(self, search_link, search_term):
-        print "== requesting", search_link
-        sparknotes_search = requests.get(search_link)
+        print "== requesting", search_link.replace(' ','+')
+        #sparknotes_search = requests.get(search_link.replace(' ','+'))
+        sparknotes_search = requests.get('http://www.sparknotes.com/cs/arrays/intro/summary.html')
         #sparknotes_search = requests.get("http://www.sparknotes.com/search?q=" + search_term)
         soup = BeautifulSoup(sparknotes_search.text, 'html.parser') 
 
         # for printing
         #html = soup.prettify()
         #html = html.encode('UTF-8')
-        
+        print '==============================================================================================='
+        #print soup.get_text()
+
         # find all the links to the individual sparknote pages that correspond to the given topic
-        for line in [link for link in soup.find_all('a') if search_term in link.text]:
+        for line in [link for link in soup.find_all('p')]: # if '.html' in link]: [link for link in soup.find_all('a') if search_term in link.text]:
             print '\n'
             print soup.title
             print line.text
             print line.get('href')
+            if raw_input('Should I print the a tags? (y/n): ') == 'y':
+                print 
             return self.crawl(line.get('href'), search_term)
 
-crawler = SparknotesCrawler(['consciousness', 'brain', 'ai', 'intelligence', 'explosions', 'bears', 'beets', 'battlestar', 'galactica'])
+crawler = SparknotesCrawler(['science'])
 for term in crawler.terms:
     print "\ncrawling for", term
     crawler.crawl("http://www.sparknotes.com/search?q=" + term, term)
